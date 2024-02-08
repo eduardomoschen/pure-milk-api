943ee7d02ace4d74f2ff88cab0286d28ffbd2a3d
Eduardo Moschen <eduardo.moschen28@gmail.com>
2024-02-08 10:34:23 -0300

    :lipstick: style: ajustes de formatação para seguir as diretrizes da PEP 8

--- orders/admin.py
+++ orders/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from .models import Order, OrderItem
 
+
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     extra = 1
+
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
